bd9196bfd7cf797f88f21dbd908981e6a287a3d1
harshj5246 <jainh9712@gmail.com>
2022-09-14 18:22:01 +0530

    update cancelation page

--- project/Airlines_App/Cancelation.xaml.cs
+++ project/Airlines_App/Cancelation.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace Airlines_App
 {
@@ -19,16 +22,132 @@ namespace Airlines_App
     /// </summary>
     public partial class Cancelation : Window
     {
+        string conString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Airlines_App;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        int ticket_id =0;
         public Cancelation()
         {
             InitializeComponent();
+            Loadtickets();
+        }
+
+     
+        public void Loadtickets()
+        {
+            //dtg_GrdFlight.Visibility = Visibility.Visible;
+            //btn_Book.Visibility = Visibility.Visible;
+
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            string query = "SELECT * FROM Booking where UserName like '" + Login.username + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dtg_GrdTicket.ItemsSource = dt.DefaultView;
+            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+            if(dt.Rows.Count > 0)
+            {
+                ticket_id = int.Parse(ds.Tables[0].Rows[0]["ticket_id"].ToString());
+
+            }
+            //else{
+            //    MessageBox.Show("No Booked Tickets Avlable");
+
+            //}
+
+            con.Close();
+        }
+
+        private void btn_dashboard_Click(object sender, RoutedEventArgs e)
+        {
+           Dashboard dashboard = new Dashboard();
+            this.Visibility = Visibility.Collapsed;
+            dashboard.Show();
         }
 
         private void btn_dashbord_Click(object sender, RoutedEventArgs e)
         {
-        //    Dashboard dashboard1 = new Dashboard();
-        //    this.Visibility = Visibility.Collapsed;
-        //    dashboard1.Show();
+            Login login = new Login();
+            this.Visibility = Visibility.Collapsed;
+            login.Show();
+        }
+
+      
+
+        private void btn_Calcle_Click(object sender, RoutedEventArgs e)
+        {
+            if (ticket_id == 0)
+            {
+                MessageBox.Show("No Booked Tickets Avilable for cancle");
+
+            }
+            else
+            {
+
+                SqlConnection con = new SqlConnection(conString);
+                con.Open();
+
+                string que = ("delete from Booking where ticket_id = " + ticket_id);
+                SqlCommand cmd = new SqlCommand(que, con);
+
+                if (MessageBox.Show("Do you really want to Cancle", "Confirmation", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+                {
+
+                    return;
+                }
+                else
+                {
+
+
+                    int no1 = seats();
+                    int editno = no1 + int.Parse(txt_noOfTickets.Text);
+
+                    SqlConnection fcon = new SqlConnection(conString);
+                    fcon.Open();
+                    string query = ("Update Flight set  Seat_Left = " + editno + " Where flight_id ='" + txt_flight_id.Text + "'");
+
+
+                    SqlCommand cmmd = new SqlCommand(query, con);
+
+                    cmmd.ExecuteNonQuery();
+                    fcon.Close();
+                    cmmd.Dispose();
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Ticket Cancled Successfully");
+
+
+                }
+                con.Close();
+                cmd.Dispose();
+            }
+            
+        }
+
+        public int seats()
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string flightque = ("Select Seat_Left from Flight where flight_id = '" + txt_flight_id.Text + "'" );
+
+            SqlDataAdapter flightadapter = new SqlDataAdapter(flightque, con);
+
+            DataSet ds = new DataSet();
+
+            flightadapter.Fill(ds);
+
+            int no = Convert.ToInt32(ds.Tables[0].Rows[0]["Seat_Left"]);
+            con.Close();
+            return no;
+            
         }
     }
 }
