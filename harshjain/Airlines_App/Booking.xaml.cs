9764523e101efa29a638bc55912de14cc2955719
Maheswaran <pystud19@gmail.com>
2022-09-14 13:04:34 +0530

    NOTES

---
+++ harshjain/Airlines_App/Booking.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Airlines_App
+{
+    /// <summary>
+    /// Interaction logic for Booking.xaml
+    /// </summary>
+    public partial class Booking : Window
+    {
+        public Booking()
+        {
+            InitializeComponent();
+            ReadFields();
+            txt_usernamebk.Text = Login.username;
+
+            
+        }
+        string conString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Airlines_App;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public void ReadFields()
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string selectQuery = "select * from Flight where flight_id = '" + Dashboard.flightid + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(selectQuery, con);
+
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+
+            txt_fightid.Text = ds.Tables[0].Rows[0]["flight_id"].ToString();
+            txt_destinationbk.Text = ds.Tables[0].Rows[0]["designation"].ToString();
+            txt_sourcebk.Text = ds.Tables[0].Rows[0]["source"].ToString();
+
+            con.Close();
+            
+
+        }
+
+
+            private void btn_dashboard_Click(object sender, RoutedEventArgs e)
+         {
+            Dashboard dashboard = new Dashboard();
+            this.Visibility = Visibility.Collapsed;
+            dashboard.Show();
+        }
+
+        private void BtnBook_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("l");
+        }
+
+        private void btn_dashbord_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            this.Visibility = Visibility.Collapsed;
+            login.Show();
+        }
+    }
+}
---
+++ harshjain/Airlines_App/Dashboard.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Airlines_App
+{
+    
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        string conString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Airlines_App;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static string flightid;
+        
+
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            lblwelcome.Content += Login.username;
+
+        }
+
+
+      
+
+        private void btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_source.Text == String.Empty)
+            {
+                txt_source.Focus();
+                MessageBox.Show("fill the Source  fields");
+                return;
+
+            }
+            else if (txt_destination.Text == String.Empty)
+            {
+                txt_destination.Focus();
+                MessageBox.Show("fill the Destination fields");
+                return;
+
+            }
+            else
+            {
+                //source = txt_source.Text;
+                //destination = txt_destination.Text;
+                //date = dp_date.Text;
+
+                //FlightList flightList = new FlightList();
+                //this.Visibility = Visibility.Collapsed;
+                //flightList.Show();
+
+                dtg_GrdFlight.Visibility = Visibility.Visible;
+                btn_Book.Visibility = Visibility.Visible;
+
+                SqlConnection con = new SqlConnection(conString);
+                con.Open();
+
+                string query = "SELECT flight_id,Airline_name,source,designation,seat_capacity,depature,arraival_time,flight_charge FROM Flight where source like '" + txt_source.Text + "' and designation like '" + txt_destination.Text+ "'";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dtg_GrdFlight.ItemsSource = dt.DefaultView;
+                
+                con.Close();
+
+            }
+        }
+
+        private void btn_logout_Click(object sender, RoutedEventArgs e)
+        {
+            lblwelcome.Content = "";
+            Login login = new Login();
+            this.Visibility = Visibility.Collapsed;
+            login.Show();
+        }
+
+        private void btn_Book_Click(object sender, RoutedEventArgs e)
+        {
+            flightid = txt_flightid.Text;
+            Booking booking = new Booking();
+            this.Visibility = Visibility.Collapsed;
+            booking.Show();
+        }
+    }
+}
