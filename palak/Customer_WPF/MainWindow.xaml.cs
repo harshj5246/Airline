11a2994fd757e2e8894ba7775cc4191a40faa36c
harshj5246 <jainh9712@gmail.com>
2022-09-17 13:00:49 +0530

    palak

---
+++ palak/Customer_WPF/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Customer_WPF.DAO;
+using Customer_WPF.Model;
+using EmployeeWpfMvvmApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Customer_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mdl = (EmployeeViewModel)MyLayout.DataContext;
+
+        }
+        public static MainWindow Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MainWindow();
+                }
+                return instance;
+            }
+        }
+        public static MainWindow instance;
+        private EmployeeViewModel mdl;
+
+        bool isNew = true;
+        public bool IsNew
+        {
+            get { return isNew; }
+            set
+            {
+                isNew = value;
+                LblFormStatus.Content = isNew ? "New Employee" : "Edit Employee";
+                TxtID.IsReadOnly = !isNew;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to save?", "Confirm", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+            {
+                return;
+            }
+
+
+            if (IsNew)
+            {
+                CustomerDAO.Create(mdl.EmployeeForm);
+            }
+            {
+                CustomerDAO.Update(mdl.EmployeeForm);
+            }
+
+            MessageBox.Show("supplier is saved successfully.");
+            lblStatus.Text = "supplier is saved successfully.";
+
+            BtnLoad_Click(null, null);
+            BtnNew_Click(null, null);
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+             ObservableCollection<CustomerModel> customer = CustomerDAO.ReadAll();
+            mdl.EmployeesGrid = customer;
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            mdl.EmployeeForm = new CustomerModel();
+            IsNew = true;
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            mdl.EmployeeForm = (CustomerModel)GrdEmployees.SelectedItem;
+            IsNew = false;
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to delete?", "Confirm", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+            {
+                return;
+            }
+            CustomerModel employee = (CustomerModel)GrdEmployees.SelectedItem;
+            CustomerDAO.Delete(employee.Id);
+
+            MessageBox.Show("supplier is deleted successfully.");
+            lblStatus.Text = "supplier is deleted successfully.";
+
+            BtnLoad_Click(null, null);
+            BtnNew_Click(null, null);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.Show();
+            this.Hide();
+        }
+
+        private void pri_Checked(object sender, RoutedEventArgs e)
+        {
+            if (pri.IsChecked == true)
+            {
+                Txtsupliyer.Text = "Prilivige";
+            }
+        }
+
+        private void normal_Checked(object sender, RoutedEventArgs e)
+        {
+            if (normal.IsChecked == true)
+            {
+                Txtsupliyer.Text = "Normal";
+            }
+        }
+    }
+}
---
+++ palak/Customer_WPF/Model/CustomerModel.cs
@@ -0,0 +1,72 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Customer_WPF.Model
+//{
+
+//    public class CustomerModel
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public double Creadit_limit { get; set; }
+//        public string Com_Address { get; set; }
+//        public string Billing_Address { get; set; }
+
+
+//        public CustomerModel() : this(0, string.Empty,0, string.Empty, string.Empty)
+//        {
+
+//        }
+
+//        public CustomerModel(int id, string name, double creadit_Limit, string com_Address, string billing_Address)
+//        {
+//            Id = id;
+//            Name = name;
+//            Creadit_limit = creadit_Limit;
+//            Com_Address = com_Address;
+//            Billing_Address = billing_Address;
+//        }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_WPF.Model
+{
+
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Creadit_limit { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string supliyer_type { get; set; }
+
+
+
+
+
+        public CustomerModel() : this(0, string.Empty, 0, string.Empty, string.Empty,string.Empty)
+        {
+
+        }
+
+        public CustomerModel(int id, string name, double creadit_limit, string address, string email, string supliyer_type)
+        {
+            Id = id;
+            Name = name;
+            Creadit_limit = creadit_limit;
+            Address = address;
+            Email = email;
+            this.supliyer_type = supliyer_type;
+        }
+    }
+}
