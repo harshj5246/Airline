6ce4d1e75787d05d3a7e311a0d26383fb862a952
harshj5246 <jainh9712@gmail.com>
2022-10-26 22:29:40 +0530

    finall

--- project/Airlines_App/Register.xaml.cs
+++ project/Airlines_App/Register.xaml.cs
@@ -34,6 +34,7 @@ namespace Airlines_App
         public Register()
         {
             InitializeComponent();
+            Dp_Dob.DisplayDateEnd = DateTime.Now;
         }
 
         string gender = "";
@@ -50,116 +51,128 @@ namespace Airlines_App
 
         private void lb_register_Click(object sender, RoutedEventArgs e)
         {
-           
-                if (txt_username.Text == String.Empty)
-                {
-                    txt_username.Focus();
-                    MessageBox.Show("fill the username  fields");
-                    return;
-                }
-                else if (string.IsNullOrEmpty(txt_fullname.Text.Trim()))
 
-                {
+            if (txt_username.Text == String.Empty)
+            {
+                txt_username.Focus();
+                MessageBox.Show("fill the username  fields");
+                return;
+            }
+            else if (string.IsNullOrEmpty(txt_fullname.Text.Trim()))
 
-                    MessageBox.Show("fill the full name  fields");
+            {
 
-                    txt_fullname.Text = String.Empty;
+                MessageBox.Show("fill the full name  fields");
 
-                    return;
+                txt_fullname.Text = String.Empty;
 
-                }
+                return;
 
-                else if (txt_fullname.Text.StartsWith("."))
+            }
 
-                {
+            else if (txt_fullname.Text.StartsWith("."))
 
-                    MessageBox.Show("Value can not start with .");
+            {
 
-                    txt_fullname.Text = String.Empty;
+                MessageBox.Show("Value can not start with .");
 
-                    return;
+                txt_fullname.Text = String.Empty;
 
-                }
+                return;
 
-                else if (txt_fullname.Text.StartsWith(" "))
+            }
 
-                {
+            else if (txt_fullname.Text.StartsWith(" "))
 
-                    MessageBox.Show(" Value can not start with blank space");
+            {
 
-                    txt_fullname.Text = String.Empty;
+                MessageBox.Show(" Value can not start with blank space");
 
-                    return;
+                txt_fullname.Text = String.Empty;
 
-                }
+                return;
 
-
-                else if (System.Text.RegularExpressions.Regex.IsMatch(txt_fullname.Text, "[^a-z A-z]"))
-                {
-                    txt_fullname.Focus();
-                    MessageBox.Show("fill the names only in fullname fields");
-                    return;
-                }
-                else if (txt_email.Text.StartsWith(" "))
-
-                {
-
-                    MessageBox.Show(" Value can not start with blank space");
-
-                    txt_fullname.Text = String.Empty;
-
-                    return;
-
-                }
-                else if (txt_email.Text == String.Empty)
-                {
-                    txt_email.Focus();
-                    MessageBox.Show("fill the email  fields");
-                    return;
-                }
-
-                else if (txt_phonenumber.Text == String.Empty)
-                {
-                    txt_phonenumber.Focus();
-                    MessageBox.Show("fill the phonenumber fields");
-                    return;
-                }
-                else if (System.Text.RegularExpressions.Regex.IsMatch(txt_phonenumber.Text, "[^0-9]"))
-                {
-                    MessageBox.Show("Please enter only numbers in phonenumber fields.");
-                    txt_phonenumber.Text = String.Empty;
-
-                    txt_phonenumber.Focus();
-                    return;
-                }
-                else if (txt_phonenumber.Text.Length != 10)
-                {
-                    txt_phonenumber.Focus();
-                    MessageBox.Show("phonenumber must be 10 digits");
-                    return;
-                }
-                else if ((rdo_female.IsChecked == false) && (rdo_male.IsChecked == false) && (rdo_prefernotosay.IsChecked == false))
-                {
-                    rdo_female.Focus();
-                    MessageBox.Show("select the gender  fields");
-                    return;
+            }
 
 
-                }
-                else if (p_password.Password.ToString() == String.Empty)
-                {
-                    p_password.Focus();
-                    MessageBox.Show("fill the password fields");
-                    return;
-                }
-                else if (p_password.Password.Length < 6)
-                {
-                    p_password.Focus();
-                    MessageBox.Show(" password must be more than 5 letters");
-                    return;
+            else if (System.Text.RegularExpressions.Regex.IsMatch(txt_fullname.Text, "[^a-z A-z]"))
+            {
+                txt_fullname.Focus();
+                MessageBox.Show("fill the names only in fullname fields");
+                return;
+            }
+            else if (txt_email.Text.StartsWith(" "))
 
-                }
-           
+            {
+
+                MessageBox.Show(" Value can not start with blank space");
+
+                txt_fullname.Text = String.Empty;
+
+                return;
+
+            }
+            else if (txt_email.Text == String.Empty)
+            {
+                txt_email.Focus();
+                MessageBox.Show("fill the email  fields");
+                return;
+            }
+            else if (!(txt_email.Text.Contains("@")))
+            {
+                txt_email.Focus();
+                MessageBox.Show("you entered incorrect email format in  the email  fields");
+                return;
+            }
+            else if (!(txt_email.Text.EndsWith(".com")))
+            {
+                txt_email.Focus();
+                MessageBox.Show("you entered incorrect email format in  the email  fields");
+                return;
+            }
+
+            else if (txt_phonenumber.Text == String.Empty)
+            {
+                txt_phonenumber.Focus();
+                MessageBox.Show("fill the phonenumber fields");
+                return;
+            }
+            else if (System.Text.RegularExpressions.Regex.IsMatch(txt_phonenumber.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers in phonenumber fields.");
+                txt_phonenumber.Text = String.Empty;
+
+                txt_phonenumber.Focus();
+                return;
+            }
+            else if (txt_phonenumber.Text.Length != 10)
+            {
+                txt_phonenumber.Focus();
+                MessageBox.Show("phonenumber must be 10 digits");
+                return;
+            }
+            else if ((rdo_female.IsChecked == false) && (rdo_male.IsChecked == false) && (rdo_prefernotosay.IsChecked == false))
+            {
+                rdo_female.Focus();
+                MessageBox.Show("select the gender  fields");
+                return;
+
+
+            }
+            else if (p_password.Password.ToString() == String.Empty)
+            {
+                p_password.Focus();
+                MessageBox.Show("fill the password fields");
+                return;
+            }
+            else if (p_password.Password.Length < 6)
+            {
+                p_password.Focus();
+                MessageBox.Show(" password must be more than 5 letters");
+                return;
+
+            }
+
             else if (p_confirmpassword.Password.ToString() == String.Empty)
             {
                 p_confirmpassword.Focus();
@@ -199,12 +212,12 @@ namespace Airlines_App
                 Clear();
 
 
-                
+
 
                 Login login = new Login();
                 this.Visibility = Visibility.Collapsed;
                 login.Show();
-               
+
             }
 
                
